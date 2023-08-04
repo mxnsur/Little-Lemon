66fd2b76fe3d5782ccc5ba35370e88c245d0bdde
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-04 17:40:18 -0300

    WelcomeScreen

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import LittleLemonHeader from './src/componentes/LittleLemonHeader'
 import LittleLemonFooter from './src/componentes/LittleLemonFooter'
+import WelcomeScreen from './src/WelcomeScreen'
 
 export default function App() {
   return (
     <>
     <View style={{flex: 1, backgroundColor: '#495E57', }}>
       <LittleLemonHeader />
+      <WelcomeScreen />
       </View>
     <View style={{ backgroundColor: '#495E57' }}>
     <LittleLemonFooter />
