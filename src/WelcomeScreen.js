e63131cf31cb30b7c4e1cf70f70b0f4551db5872
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-04 19:35:14 -0300

    Adicionado StyleSheet no WelcomeScreen

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,29 +1,13 @@
 import React from "react";
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
-     <ScrollView
-        indicatorStyle={"white"}
-        style={{
-          flex: 1,
-        }}>
-        <Text
-        style={{ 
-            padding: 40, 
-            fontSize: 35, 
-            color: '#EDEFEE',
-            textAlign: "center" }}>
+     <ScrollView indicatorStyle={"white"} style={WelcomeScreenStyles.containerScroll}>
+        <Text style={WelcomeScreenStyles.WelcomeText}>
         Welcome to Little Lemon
         </Text>
-        <Text
-        style={{
-          fontSize: 25,
-          padding: 20,
-          marginVertical: 8,
-          color: '#EDEFEE',
-          textAlign: 'center',
-        }}>
+        <Text style={WelcomeScreenStyles.itemText}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear your experience with us!
@@ -31,3 +15,22 @@ export default function WelcomeScreen() {
         </ScrollView>
     )
 }
+
+const WelcomeScreenStyles = StyleSheet.create({
+    containerScroll: {
+        flex: 1,
+    },
+    WelcomeText: {
+        padding: 40, 
+        fontSize: 35, 
+        color: '#EDEFEE',
+        textAlign: "center",
+    },
+    itemText: {
+        fontSize: 25,
+          padding: 20,
+          marginVertical: 8,
+          color: '#EDEFEE',
+          textAlign: 'center',
+    },
+  });
