afe12e4914659e94dcb5cf047b638939f4633b8d
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-04 17:50:12 -0300

    Testando ScrollView e Criando Arrays

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native'
 import LittleLemonHeader from './src/componentes/LittleLemonHeader'
 import LittleLemonFooter from './src/componentes/LittleLemonFooter'
 import WelcomeScreen from './src/WelcomeScreen'
+import MenuItems from './src/MenuItems'
 
 export default function App() {
   return (
     <>
     <View style={{flex: 1, backgroundColor: '#495E57', }}>
       <LittleLemonHeader />
-      <WelcomeScreen />
+      <MenuItems />
       </View>
     <View style={{ backgroundColor: '#495E57' }}>
     <LittleLemonFooter />
