85cef953f0caa93e7511963c2586783f2144a0c5
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-05 20:00:52 -0300

    Re

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,36 +1,57 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
 
 export default function WelcomeScreen() {
     return (
      <ScrollView indicatorStyle={"white"} style={WelcomeScreenStyles.containerScroll}>
-        <Text style={WelcomeScreenStyles.WelcomeText}>
-        Welcome to Little Lemon
-        </Text>
+        <View style={WelcomeScreenStyles.headerWrapper}>
+        <Image
+          style={WelcomeScreenStyles.image}
+          source={require('../src/img/logo.png')}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
+        />
+
+        <Text style={WelcomeScreenStyles.headerText}>Little Lemon</Text>
+      </View>
         <Text style={WelcomeScreenStyles.regularText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear your experience with us!
+        Little Lemon é um charmoso bistrô de bairro que serve comida simples
+        e cocktails clássicos num ambiente animado mas casual. Vamos adorar 
+        ouvir sua experiência conosco!
         </Text>
         </ScrollView>
     )
 }
 
 const WelcomeScreenStyles = StyleSheet.create({
-    containerScroll: {
+    container: {
         flex: 1,
-    },
-    WelcomeText: {
-        padding: 40, 
-        fontSize: 35, 
+      },
+      headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        margin: 10,
+      },
+      headerText: {
+        paddingRight: 10,
+        paddingLeft: 20,
+        paddingTop: 30,
+        paddingBottom: 10,
+        fontSize: 30,
         color: '#EDEFEE',
-        textAlign: "center",
-    },
-    regularText: {
-        fontSize: 25,
-          padding: 20,
-          marginVertical: 8,
-          color: '#EDEFEE',
-          textAlign: 'center',
-    },
-  });
+        textAlign: 'center',
+      },
+      regularText: {
+        fontSize: 24,
+        padding: 20,
+        marginVertical: 8,
+        color: '#EDEFEE',
+        textAlign: 'center',
+      },
+      image: {
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+      },
+    });
