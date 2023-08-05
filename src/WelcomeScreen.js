48795103458100df7a059605c032b38c4e55d2a8
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-05 20:19:14 -0300

    Ativando DarkMode

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -1,57 +1,79 @@
-import React from "react";
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import {ScrollView, View, Text, StyleSheet, Image, useColorScheme, } from 'react-native';
 
 export default function WelcomeScreen() {
-    return (
-     <ScrollView indicatorStyle={"white"} style={WelcomeScreenStyles.containerScroll}>
-        <View style={WelcomeScreenStyles.headerWrapper}>
+  const colorScheme = useColorScheme();
+
+  return (
+    <ScrollView 
+      style={[
+        styles.container,
+        colorScheme === 'light'
+          ? { backgroundColor: '#fff' }
+          : { backgroundColor: '#333333' },
+      ]}>
+      <View style={styles.headerWrapper}>
         <Image
-          style={WelcomeScreenStyles.image}
-          source={require('../src/img/logo.png')}
+          style={styles.image}
+          source={require('./img/logo.png')}
           resizeMode="cover"
           accessible={true}
           accessibilityLabel={'Little Lemon Logo'}
         />
 
-        <Text style={WelcomeScreenStyles.headerText}>Little Lemon</Text>
+        <Text
+          style={[
+            styles.headerText,
+            colorScheme === 'light'
+              ? { color: '#333333' }
+              : { color: '#EDEFEE' },
+          ]}>
+          Little Lemon
+        </Text>
       </View>
-        <Text style={WelcomeScreenStyles.regularText}>
+      <Text
+          style={[
+            styles.regularText,
+            colorScheme === 'light'
+              ? { color: '#333333' }
+              : { color: '#EDEFEE' },
+          ]}>
         Little Lemon é um charmoso bistrô de bairro que serve comida simples
         e cocktails clássicos num ambiente animado mas casual. Vamos adorar 
         ouvir sua experiência conosco!
-        </Text>
-        </ScrollView>
-    )
+      </Text>
+    </ScrollView>
+  );
 }
 
-const WelcomeScreenStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-      },
-      headerWrapper: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        margin: 10,
-      },
-      headerText: {
-        paddingRight: 10,
-        paddingLeft: 20,
-        paddingTop: 30,
-        paddingBottom: 10,
-        fontSize: 30,
-        color: '#EDEFEE',
-        textAlign: 'center',
-      },
-      regularText: {
-        fontSize: 24,
-        padding: 20,
-        marginVertical: 8,
-        color: '#EDEFEE',
-        textAlign: 'center',
-      },
-      image: {
-        width: 100,
-        height: 100,
-        borderRadius: 20,
-      },
-    });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  headerText: {
+    paddingRight: 10,
+    paddingLeft: 20,
+    paddingTop: 30,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: '#EDEFEE',
+    textAlign: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+  },
+});
