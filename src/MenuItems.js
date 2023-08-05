c1df0377896720a27a02406c6840d7f0210cf435
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-05 00:05:50 -0300

    adicionando o preço nas sessões

--- src/MenuItems.js
+++ src/MenuItems.js
@@ -3,37 +3,47 @@ import { View, Text, StyleSheet, SectionList} from 'react-native';
 
 const menuItemsToDisplay = [
     {
-      title: 'Appetizers',
-      data: [
-        'Hummus',
-        'Moutabal',
-        'Falafel',
-        'Marinated Olives',
-        'Kofta',
-        'Eggplant Salad',
-      ],
-    },
-    {
-      title: 'Main Dishes',
-      data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
-    },
-    {
-      title: 'Sides',
-      data: [
-        'Fries',
-        'Buttered Rice',
-        'Bread Sticks',
-        'Pita Pocket',
-        'Lentil Soup',
-        'Greek Salad',
-        'Rice Pilaf',
-      ],
-    },
-    {
-      title: 'Desserts',
-      data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
-    },
-  ];
+        title: 'Appetizers',
+        data: [
+          { name: 'Hummus', price: '$5.00' },
+          { name: 'Moutabal', price: '$5.00' },
+          { name: 'Falafel', price: '$7.50' },
+          { name: 'Marinated Olives', price: '$5.00' },
+          { name: 'Kofta', price: '$5.00' },
+          { name: 'Eggplant Salad', price: '$8.50' },
+        ],
+      },
+      {
+        title: 'Main Dishes',
+        data: [
+          { name: 'Lentil Burger', price: '$10.00' },
+          { name: 'Smoked Salmon', price: '$14.00' },
+          { name: 'Kofta Burger', price: '$11.00' },
+          { name: 'Turkish Kebab', price: '$15.50' },
+        ],
+      },
+      {
+        title: 'Sides',
+        data: [
+          { name: 'Fries', price: '$3.00', id: '11K' },
+          { name: 'Buttered Rice', price: '$3.00' },
+          { name: 'Bread Sticks', price: '$3.00' },
+          { name: 'Pita Pocket', price: '$3.00' },
+          { name: 'Lentil Soup', price: '$3.75' },
+          { name: 'Greek Salad', price: '$6.00' },
+          { name: 'Rice Pilaf', price: '$4.00' },
+        ],
+      },
+      {
+        title: 'Desserts',
+        data: [
+          { name: 'Baklava', price: '$3.00' },
+          { name: 'Tartufo', price: '$3.00' },
+          { name: 'Tiramisu', price: '$5.00' },
+          { name: 'Panna Cotta', price: '$5.00' },
+        ],
+      },
+    ];
   
   const Item = ({ name, price }) => (
     <View style={menuStyles.innerContainer}>
@@ -42,21 +52,23 @@ const menuItemsToDisplay = [
     </View>
   );
   
-  const Separator = () => <View style={menuStyles.separator} />;
-  
-  const Footer = () => (
-    <Text style={menuStyles.footerText}>
-      All Rights Reserved by Little Lemon
-    </Text>
-  );
-  
   const MenuItems = () => {
-    const renderItem = ({ item }) => <Item name={item} />;
+    const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
   
     const renderSectionHeader = ({ section: { title } }) => (
-      <Text style={menuStyles.sectionHeader}>{title} </Text>
+      <View style={menuStyles.headerStyle}>
+        <Text style={menuStyles.sectionHeader}>{title}</Text>
+      </View>
     );
   
+   const Separator = () => <View style={menuStyles.separator} />;
+  
+   const Footer = () => (
+      <Text style={menuStyles.footerText}>
+        All Rights Reserved by Little Lemon
+      </Text>
+    );
+
     return (
       <View style={menuStyles.container}>
         <SectionList
@@ -65,20 +77,21 @@ const menuItemsToDisplay = [
           renderItem={renderItem}
           renderSectionHeader={renderSectionHeader}
           ListFooterComponent={Footer}
-          ItemSeparatorComponent={Separator}></SectionList>
+          ItemSeparatorComponent={Separator}>
+        </SectionList>
       </View>
     );
   };
   
-  // Add styles to the component
   const menuStyles = StyleSheet.create({
     container: {
       flex: 1,
     },
     innerContainer: {
-      paddingHorizontal: 40,
-      paddingVertical: 20,
-      backgroundColor: '#333333',
+        paddingHorizontal: 40,
+        paddingVertical: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     sectionHeader: {
       backgroundColor: '#fbdabb',
@@ -97,7 +110,7 @@ const menuItemsToDisplay = [
     },
     footerText: {
       color: '#EDEFEE',
-      fontSize: 20,
+      fontSize: 1,
       flexWrap: 'wrap',
       textAlign: 'center',
     },
