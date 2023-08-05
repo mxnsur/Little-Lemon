c944a674955ee4c7885e70a406691ee929d75377
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-05 17:44:48 -0300

    Criação de botão para fazer login

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
     <>
     <View style={styles.container}>
       <LittleLemonHeader />
-      <MenuItems />
+      <LoginScreen />
     </View>
     </>
   );
--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { Text, StyleSheet, TextInput, ScrollView, Pressable} from 'react-native';
 
-const LoginScreen = () => { 
+export default function LoginScreen() {
       const [email, onChangeEmail] = useState(''); 
       const [password, onChangePassword] = useState(''); 
+      const [loggedIn, onLogin] = useState(true);
 
 return (
     <ScrollView keyboardDismissMode="on-drag" style={styles.container}>
         <Text style={styles.headerText}>Bem-vindo(a) ao Little Lemon</Text>
-        <Text style={styles.regularText}>Faça o login para continuar </Text>
+
+        {loggedIn && 
+        <Text style={styles.regularText}>Você está logado!</Text>}
+
+        {!loggedIn && (
+         <>
+         <Text style={styles.regularText}>Faça o login para continuar</Text>
         <TextInput 
         style={styles.input} 
         value={email} 
@@ -25,9 +32,14 @@ return (
         placeholder={'Senha'} 
         keyboardType={'default'}
         clearButtonMode={'always'}
-        /> 
+        />
+        <Pressable onPress={() => onLogin(!loggedIn)} style={styles.button}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </Pressable>
+        </>
+      )}
     </ScrollView>
-)
+  );
 }
 
 const styles = StyleSheet.create({
@@ -56,8 +68,20 @@ const styles = StyleSheet.create({
         borderColor: '#EDEFEE', 
         backgroundColor: 'white', 
         }, 
+    button: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 100,
+        backgroundColor: '#EE9972',
+        borderColor: '#EE9972',
+        borderWidth: 2,
+        borderRadius: 50,
+        },
+    buttonText: {
+        color: 'black',
+        textAlign: 'center',
+        fontSize: 25,
+        },    
   });
-
-export default LoginScreen
-
   
