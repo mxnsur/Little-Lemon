3c2eb0e990e3d3c7980c133bcc3494917a4f1292
Rafael Mansur <rafaelmansursn@gmail.com>
2023-08-06 18:32:19 -0300

    Testando TabNavigation

--- App.js
+++ App.js
@@ -4,40 +4,38 @@ import { View, Text, StyleSheet } from 'react-native';
 import LoginScreen from './src/telas/LoginScreen';
 import Welcome from './src/telas/Welcome';
 import Cardapio from './src/telas/Cardapio';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 export default function App() {
   return (
-    <>
-      <NavigationContainer>
-        <View style={styles.container}>
-          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerStyle: {backgroundColor: '#FBDABB'} }}>
-            <Stack.Screen 
-            options= {{ title: 'Login' }} 
-            name="Login" 
-            component={LoginScreen} 
-            />
-            <Stack.Screen 
-            options={{ title: 'Menu' }} 
-            name="Menu" 
-            component={Welcome} 
-            />
-            <Stack.Screen 
-            options= {{ title: 'Cardapio' }} 
-            name="Cardapio" 
-            component={Cardapio} 
-            />
-          </Stack.Navigator>
-        </View>
-        <View style={styles.footerContainer}>
-        </View>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Menu') {
+            iconName = focused
+              ? 'information-circle'
+              : 'information-circle-outline';
+          } else if (route.name === 'Cardapio') {
+            iconName =  'list-outline';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'grey',
+      })}>
+      <Tab.Screen name="Menu" component={Welcome} />
+      <Tab.Screen name="Cardapio" component={Cardapio} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
